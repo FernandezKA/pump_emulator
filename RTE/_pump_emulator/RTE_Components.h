0f41a3295e72ada243f244d1d70cab91b7bb0732
Kirill Fernandez <fernandes.kir@yandex.ru>
2022-04-04 13:23:05 +0300

    Successfull compilation with FreeRTOS

---
+++ RTE/_pump_emulator/RTE_Components.h
@@ -0,0 +1,51 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'pump_emulator' 
+ * Target:  'pump_emulator' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "gd32f10x.h"
+
+/* ARM.FreeRTOS::RTOS:Config:FreeRTOS:10.4.6 */
+#define RTE_RTOS_FreeRTOS_CONFIG        /* RTOS FreeRTOS Config for FreeRTOS API */
+/* ARM.FreeRTOS::RTOS:Core:Cortex-M:10.4.6 */
+#define RTE_RTOS_FreeRTOS_CORE          /* RTOS FreeRTOS Core */
+/* ARM.FreeRTOS::RTOS:Coroutines:10.4.6 */
+#define RTE_RTOS_FreeRTOS_COROUTINE     /* RTOS FreeRTOS Co-routines */
+/* ARM.FreeRTOS::RTOS:Event Groups:10.4.6 */
+#define RTE_RTOS_FreeRTOS_EVENTGROUPS   /* RTOS FreeRTOS Event Groups */
+/* ARM.FreeRTOS::RTOS:Heap:Heap_4:10.4.6 */
+#define RTE_RTOS_FreeRTOS_HEAP_4        /* RTOS FreeRTOS Heap 4 */
+/* ARM.FreeRTOS::RTOS:Message Buffer:10.4.6 */
+#define RTE_RTOS_FreeRTOS_MESSAGE_BUFFER /* RTOS FreeRTOS Message Buffers */
+/* ARM.FreeRTOS::RTOS:Stream Buffer:10.4.6 */
+#define RTE_RTOS_FreeRTOS_STREAM_BUFFER /* RTOS FreeRTOS Stream Buffers */
+/* ARM.FreeRTOS::RTOS:Timers:10.4.6 */
+#define RTE_RTOS_FreeRTOS_TIMERS        /* RTOS FreeRTOS Timers */
+/* GigaDevice::Device:GD32F10x_StdPeripherals:FMC:2.0.1 */
+#define RTE_DEVICE_STDPERIPHERALS_FMC
+/* GigaDevice::Device:GD32F10x_StdPeripherals:GPIO:2.0.1 */
+#define RTE_DEVICE_STDPERIPHERALS_GPIO
+/* GigaDevice::Device:GD32F10x_StdPeripherals:MISC:2.0.1 */
+#define RTE_DEVICE_STDPERIPHERALS_MISC
+/* GigaDevice::Device:GD32F10x_StdPeripherals:PMU:2.0.1 */
+#define RTE_DEVICE_STDPERIPHERALS_PMU
+/* GigaDevice::Device:GD32F10x_StdPeripherals:RCU:2.0.1 */
+#define RTE_DEVICE_STDPERIPHERALS_RCU
+/* GigaDevice::Device:GD32F10x_StdPeripherals:TIMER:2.0.1 */
+#define RTE_DEVICE_STDPERIPHERALS_TIMER
+/* GigaDevice::Device:GD32F10x_StdPeripherals:USART:2.0.1 */
+#define RTE_DEVICE_STDPERIPHERALS_USART
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ includes/main.h
@@ -0,0 +1,9 @@
+#ifndef _main_h_
+#define _main_h_
+
+#include "gd32f10x.h"
+#include "gd32f10x_timer.h"
+#include "gd32f10x_usart.h"
+#include "gd32f10x_gpio.h"
+
+#endif
